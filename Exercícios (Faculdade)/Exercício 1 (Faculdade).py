0b27e5b13ed54f5c0f225f401c1cd92a921795c9
Lawrenceofsampa <2ccoelhos@gmail.com>
2022-08-26 00:07:39 -0300

    X ao cubo

--- Exercícios (Faculdade)/Exercício 1 (Faculdade).py
+++ Exercícios (Faculdade)/Exercício 1 (Faculdade).py
@@ -1,4 +1,6 @@
 '''Crie um algoritmo que leia um valor X, calcule seu valor ao cubo e informe o resultado na saída do programa. '''
 
 while True:
-
+    X = int(input('Entre com o valor (x): '))
+    Calculo = X**3
+    print(f'O valor (x) ao cubo é: {Calculo}')
